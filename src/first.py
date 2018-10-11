593e9d3350d94d8011623fce764e8b059517d6d0
Juan Esteban Moreno López <juanes.ml89@gmail.com>
2018-10-11 02:52:05 +0200

    Update first.py

--- src/first.py
+++ src/first.py
@@ -7,7 +7,17 @@ import os
 
 
 class mi_service:
-    
-  def suma(a,b):
-    suma = a +b
-    return suma
+    """ Clase para el microservicio """
+    def fun():
+        with urllib.request.urlopen("https://newsapi.org/v2/top-headlines?sources=el-mundo&apiKey=3daa18d2a35747a4ab8a03b449c7e048") as url:
+            data = json.loads(url.read().decode('utf-8'))
+
+            # for date in data:
+            #     print (date)
+        leer = json.loads(open('datos.json').read())
+        list=[]
+        for i in leer:
+            list.append(i['description'])
+
+
+        return  '%s %s' % (list[0], list[1])
