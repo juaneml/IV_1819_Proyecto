2f0ff037d8a46450f9c8313e98dcb64cef9f4f15
juaneml <juanes.ml89@gmail.com>
2018-11-01 16:29:35 +0100

    update test

--- test/test.py
+++ test/test.py
@@ -7,12 +7,13 @@ from first import *
 
 class Test(unittest.TestCase):
 
-    pruebaArt = Articulo()#.Imprime(Articulo)
-    pruebaNot = Noticia()#.fun()
-    """ Test Nocicias """
-    def testNoticias(self):
-        self.assertEqual(self.pruebaNot.num_noticias(),self.pruebaArt.Num_arti(),"Numeros iguales")
-    	self.assertEqual(self.pruebaNot.get_titulo(0),str('German governing parties punished in state election'),"Titulo Correcto")
+    #pruebaArt = Articulo()
+    #pruebaNot = Noticia()
+
+    """Test noticias """
+    def TestNoticias(self):
+        self.assertEqual(str(self.pruebaNot.num_noticias()),str(self.pruebaArt.Num_arti()),"Coincide")
+        self.assertEqual(self.pruebaNot.get_titulo(0),str('German governing parties punished in state election'),"Titulo Correcto")
 
 if __name__ == '__main__':
     unittest.main()
