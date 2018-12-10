f8d7d3c219b725f269b7e618d37826fed7d3d015
juaneml <juanes.ml89@gmail.com>
2018-12-10 11:43:43 +0100

    datos desde el test close#14

--- test/test_.py
+++ test/test_.py
@@ -2,32 +2,41 @@
 # -*- coding: utf-8 -*-
 
 import sys
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
 import imp
 import pytest
 import unittest
 sys.path.append('../src/')
-from first import *
+from principal import *
 
 class TestMethods(unittest.TestCase):
-    pruebaArt = Articulo()
-    pruebaNot = Noticia()
+
+    with open('../json/datos_m.json','r') as noti:
+            lista_noticias = json.load(noti)
+
+    pruebaNot = Newsgroups()
+    pruebaNot.Crea_news(lista_noticias)
 
     """ Test noticia """
 
     def test_noticia(self):
-        self.assertEqual(str(self.pruebaNot.num_noticias()),str(self.pruebaArt.Num_arti()),"Coincide")
-        self.assertEqual(self.pruebaNot.get_titulo("titulo"),False,"El indice debe ser un entero")
+        self.assertEqual(self.pruebaNot.getNumNot(),20,"Se han añadido correctamente las 20 noticias") 
+        self.assertEqual(self.pruebaNot.getNoticia().get_titulo("titulo"),False,"El indice debe ser un entero")
 
     """ Test titulo """
+
     def test_titulo(self):
-        self.assertEqual(self.pruebaNot.get_titulo(0),str('German governing parties punished in state election'),"Titulo Correcto")
-        self.assertEqual(self.pruebaNot.set_titulo(0),False,"Debe ser una cadena")
+        self.assertEqual(self.pruebaNot.getNoticia().get_titulo(0),str('German governing parties punished in state election'),"Titulo Correcto")
+        self.assertEqual(self.pruebaNot.getNoticia().set_titulo(0),False,"Debe ser una cadena")
 
     """ Test titulo """
-    
+
     def test_descrip(self):
-        self.assertEqual(self.pruebaNot.get_descrip(0),str(''),"Descripcion vacia")
-        self.assertEqual(self.pruebaNot.get_descrip(1),str('The deal was just announced.'),"Descripción valida")
+        self.assertEqual(self.pruebaNot.getNoticia().get_descrip(0),str(' '),"Descripcion vacia")
+        self.assertEqual(self.pruebaNot.getNoticia().get_descrip(1),str('The deal was just announced.'),"Descripción valida")
 
 if __name__ == '__main__':
     unittest.main()
