41f0207e47a77d59543b6c69674f92c62188a33e
juaneml <juanes.ml89@gmail.com>
2019-01-02 20:11:25 +0100

    update test_app.py

--- test/test_app.py
+++ test/test_app.py
@@ -13,9 +13,20 @@ class testProyecto(unittest.TestCase):
     def test_url_status(self):
         response = requests.get(url+'/status')
         self.assertEqual(response.json()["noticias"]["ruta"],"/noticias", "Correcto")
+
     def test_url_noticias(self):
         response = requests.get(url+'/noticias')
         self.assertEqual(response.json()['Noticia']['Titulo'],"Granada, una ciudad con encanto","titulo correcto")
+
+    def test_post_noticias(self):
+        response = requests.post(url+'/method_post')
+        assert(response.status_code == 200)
+
+    def test_put_noticias(self):
+        response = requests.put(url+'method_put?noticia=una noticia&anterior=es anterior&nuevo=es nuevo')
+        assert(response.status_code == 200)
+
+
 
 if __name__ == '__main__':
 	unittest.main()
